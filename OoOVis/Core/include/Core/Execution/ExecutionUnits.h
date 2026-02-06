ce4899b662711077c22ea2e691a2324fceb71805
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-06 21:13:18 +0400

    Correct the comments that explain the fields of the Buffer_Entry

--- OoOVis/Core/include/Core/Execution/ExecutionUnits.h
+++ OoOVis/Core/include/Core/Execution/ExecutionUnits.h
@@ -32,16 +32,14 @@ namespace OoOVis
 
 		class Execution_Unit_Load_Store {
 		public:
-			static void buffer_allocation_phase(const Reservation_Station_Entry* source_entry);
-			static Forwarding_Data execute_load();
+			static Forwarding_Data	buffer_allocation_phase(const Reservation_Station_Entry* source_entry);
+			static Forwarding_Data	execute_load();
 			static void				execute_store(memory_addr_t store_id);
-			static bool				store_buffer_is_full() { return _store_buffer.size() >= STORE_BUFFER_SIZE; }
-			static bool				load_buffer_is_full() { return _load_buffer.size() >= LOAD_BUFFER_SIZE; }
 			struct Buffer_Entry {
 				u32			  self_id; // Reorder buffer will find the buffer to flush thanks to this field
-				reg_id_t      register_id; // load uses this to broadcast the value to common data bus
-				size_t	      reorder_buffer_entry_index; // load also uses this to set the rob entry free
-				data_t        register_data; // this is both used by store and load 
+				reg_id_t      register_id; // load uses this to write  the value to the regsiter file
+				size_t	      reorder_buffer_entry_index; // load  uses this to set the rob entry free
+				data_t        register_data; // this is  used by store  
 				memory_addr_t calculated_address; // this is also both used by store and load
 				u32			  producer_tag; // load uses this to broadcast the tag to the common data bus for forwarding logic
 				Buffer_Entry(
@@ -59,6 +57,10 @@ namespace OoOVis
 					producer_tag(producer_tag_){}
 				Buffer_Entry() = default;
 			};
+		private:
+			static std::pair<size_t,size_t>	find_load_that_is_executable(); // .first stands for executabel load if it can be executed by forwarding .second will hold the store buffer entry index that is being forwarded from
+			static bool						store_buffer_is_full() { return _store_buffer.size() >= STORE_BUFFER_SIZE; }
+			static bool						load_buffer_is_full() { return _load_buffer.size() >= LOAD_BUFFER_SIZE; }
 		private:
 			static std::vector<Buffer_Entry> _store_buffer;
 			static std::vector<Buffer_Entry> _load_buffer;
