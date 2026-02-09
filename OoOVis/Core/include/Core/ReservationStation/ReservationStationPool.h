35181ebd80f157f0cadac4f5a4f3cad4078c7619
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-10 01:17:58 +0400

    Change flush_mispredicted return to void

--- OoOVis/Core/include/Core/ReservationStation/ReservationStationPool.h
+++ OoOVis/Core/include/Core/ReservationStation/ReservationStationPool.h
@@ -9,7 +9,7 @@ namespace OoOVis
 	{
 		struct Reservation_Station_Pool {
 
-			static bool flush_mispredicted(memory_addr_t id); 
+			static void flush_mispredicted(memory_addr_t id); 
 			static void wakeup(u32 producer_tag, data_t produced_data);
 			static void deallocate_entry(u32 tag);
 			static Reservation_Station& get_reservation_station(RESERVATION_STATION_ID id);
