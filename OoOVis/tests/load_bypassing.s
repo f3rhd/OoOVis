3d9376ccf7b4f8c6ef678bcbac4572313fa0ced4
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-09 20:15:47 +0400

    works

--- OoOVis/tests/load_bypassing.s
+++ OoOVis/tests/load_bypassing.s
@@ -1,7 +1,13 @@
 li x1,1000
-sw x1,0(x1)
-sw x1,10(x1)
-sw x1,20(x1)
-sw x1,30(x1)
 sw x1,40(x1)
-lw x1,60(x1) # this is a bypassable load instruction since its  address doesnt alias with any of the instructions above
+sw x1,50(x1)
+sw x1,60(x1)
+sw x1,70(x1)
+sw x1,80(x1)
+sw x1,90(x1)
+sw x1,100(x1)
+sw x1,110(x1)
+sw x1,120(x1)
+sw x1,130(x1)
+sw x1,140(x1)
+lw x1,40(x1) # this is a bypassable load instruction since its  address doesnt alias with any of the instructions above
--- OoOVis/tests/loop.s
+++ OoOVis/tests/loop.s
@@ -1,4 +1,10 @@
-li x2,5
+li x2,3
 loop:
 	addi x1,x1,1
 	bne  x1,x2,loop
+mv x2,x1
+mv x4,x1
+mv x5,x1
+mv x6,x1
+mv x7,x1
+mv x8,x1
