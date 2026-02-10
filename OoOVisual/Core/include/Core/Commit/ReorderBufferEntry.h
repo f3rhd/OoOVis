3c4e7c62d0d92b08e5372a10216b78f9692b0172
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-11 00:55:00 +0400

    Reorder buffer flushes mispredicted instructions based on timestamps now

--- OoOVisual/Core/include/Core/Commit/ReorderBufferEntry.h
+++ OoOVisual/Core/include/Core/Commit/ReorderBufferEntry.h
@@ -8,11 +8,11 @@ namespace OoOVisual
 	namespace Core {
 		struct Reorder_Buffer_Entry {
 			virtual ~Reorder_Buffer_Entry() = default;
-			Reorder_Buffer_Entry(FrontEnd::FLOW_TYPE flow,u32 speculation_id_) : flow_type(flow), origin_branch_id(speculation_id_) {}
+			Reorder_Buffer_Entry(FrontEnd::FLOW_TYPE flow,u32 origin_branch_timestamp_) : flow_type(flow), origin_branch_timestamp(origin_branch_timestamp_) {}
 			virtual FrontEnd::FLOW_TYPE flow() { return flow_type; };
 		public:
 			bool ready = false;
-			u32 origin_branch_id = Constants::NOT_SPECULATIVE;
+			time_t origin_branch_timestamp = Constants::NOT_SPECULATIVE;
 			FrontEnd::FLOW_TYPE flow_type = FrontEnd::FLOW_TYPE::UNKNOWN;
 		};
 		struct Register_Reorder_Buffer_Entry : Reorder_Buffer_Entry { // load dispatching and jump will also create this type of entry
@@ -24,47 +24,47 @@ namespace OoOVisual
 				reg_id_t architectural, 
 				reg_id_t old,
 				reg_id_t new_,
-				u32 speculation_id
+				time_t timestamp_
 			) : old_alias(old), 
 				new_alias(new_), 
 				architectural_register_id(architectural), 
-				Reorder_Buffer_Entry(flow,speculation_id) {}
+				Reorder_Buffer_Entry(flow,timestamp_) {}
 		};
 		struct Store_Reorder_Buffer_Entry : Reorder_Buffer_Entry {
 			u32 store_id;
-			Store_Reorder_Buffer_Entry(FrontEnd::FLOW_TYPE flow, u32 id_,u32 speculation_id) : store_id(id_), Reorder_Buffer_Entry(flow,speculation_id) {}
+			Store_Reorder_Buffer_Entry(FrontEnd::FLOW_TYPE flow, u32 id_, time_t origin_branch_timestamp) : store_id(id_), Reorder_Buffer_Entry(flow,origin_branch_timestamp) {}
 		};
 		struct Branch_Conditional_Reorder_Buffer_Entry : Reorder_Buffer_Entry {
 
 			bool mispredicted = false;
-			memory_addr_t self_id;
+			time_t self_timestamp;
 			Branch_Conditional_Reorder_Buffer_Entry(
 				FrontEnd::FLOW_TYPE flow,
-				u32 speculation_id,
-				memory_addr_t instruction_id_
-			) : self_id(instruction_id_),
-				Reorder_Buffer_Entry(flow,speculation_id) {}
+				time_t origin_branch_timestamp,
+				time_t time_
+			) : self_timestamp(time_),
+				Reorder_Buffer_Entry(flow,origin_branch_timestamp) {}
 		};
 		struct Branch_Unconditional_Reorder_Buffer_Entry : Reorder_Buffer_Entry {
 			bool mispredicted = false;
 			reg_id_t old_alias;
 			reg_id_t new_alias;
 			reg_id_t architectural_register_id;
-			memory_addr_t self_id;
+			time_t self_timestamp;
 			Branch_Unconditional_Reorder_Buffer_Entry ( 
 				FrontEnd::FLOW_TYPE flow, 
 				reg_id_t architectural, 
 				reg_id_t old,
 				reg_id_t new_,
 				bool mispredicted_, // jalr will always be true 
-				u32 speculation_id,
-				memory_addr_t instruction_id_
+				time_t origin_branch_timestamp_,
+				time_t self_timestamp_ 
 			) : old_alias(old), 
 				new_alias(new_), 
 				architectural_register_id(architectural), 
 				mispredicted(mispredicted_),
-				self_id(instruction_id_),
-				Reorder_Buffer_Entry(flow,speculation_id) {}
+				self_timestamp(self_timestamp_),
+				Reorder_Buffer_Entry(flow,origin_branch_timestamp_) {}
 
 		};
 	} // namespace Core
---
+++ OoOVisual/Core/include/src/Commit/ReorderBuffer.cpp
@@ -0,0 +1,132 @@
+#pragma once
+#include <Core/Commit/ReorderBuffer.h>
+#include <Core/Constants/Constants.h>
+#include <Core/RegisterFile/RegisterFile.h>
+#include <Core/Execution/ExecutionUnits.h>
+#include <iostream>
+namespace OoOVisual
+{
+	namespace Core {
+
+		#define ROB_MISPREDICTION_RECOVERY(BUFFER_TYPE) {                                         \
+			auto branch_id = dynamic_cast<BUFFER_TYPE*>(entry)->self_timestamp; \
+																					   \
+			_buffer[_head].reset();                                                    \
+			_head = (_head + 1) % Constants::REORDER_BUFFER_SIZE;                                                                   \
+																					   \
+			while (                                                                    \
+				_buffer[_head] &&                                                      \
+				_buffer[_head]->origin_branch_timestamp != Constants::NOT_SPECULATIVE &&        \
+				_buffer[_head]->origin_branch_timestamp == branch_id                      \
+			) {                                                                        \
+				auto* current_entry = _buffer[_head].get();                            \
+																					   \
+				if (current_entry->flow() == FrontEnd::FLOW_TYPE::BRANCH_CONDITIONAL) { \
+					branch_id = dynamic_cast<Branch_Conditional_Reorder_Buffer_Entry*>(current_entry)->self_timestamp; \
+				}                                                                      \
+				else if (current_entry->flow() == FrontEnd::FLOW_TYPE::BRANCH_UNCONDITIONAL) { \
+					branch_id = dynamic_cast<Branch_Unconditional_Reorder_Buffer_Entry*>(current_entry)->self_timestamp; \
+				}                                                                      \
+																					   \
+				_buffer[_head].reset();                                                \
+				_head = (_head + 1) % Constants::REORDER_BUFFER_SIZE;                                                               \
+			}                                                                          \
+			Register_File::restore_alias_table();										\
+		}
+		std::vector<std::unique_ptr<Reorder_Buffer_Entry>> Reorder_Buffer::_buffer(Constants::REORDER_BUFFER_SIZE);
+		size_t Reorder_Buffer::_head{};
+		size_t Reorder_Buffer::_tail{};
+		size_t Reorder_Buffer::allocate(std::unique_ptr<Reorder_Buffer_Entry>&& entry) {
+			_buffer[_tail] = std::move(entry);
+			auto allocated_entry_index = _tail;
+			_tail = (_tail + 1) % Constants::REORDER_BUFFER_SIZE;
+			return allocated_entry_index;
+		}
+
+		void Reorder_Buffer::set_ready(u64 target_entry_index) {
+			if (target_entry_index >= Constants::REORDER_BUFFER_SIZE) {
+				std::cout << "Tried to access non-existing reorder buffer entry.\n";
+				exit(EXIT_FAILURE);
+			}
+			_buffer[target_entry_index]->ready = true;
+		}
+
+		void Reorder_Buffer::set_branch_evaluation(u64 target, bool was_misprediction)
+		{
+			if (target >= Constants::REORDER_BUFFER_SIZE) {
+				std::cout << "Tried to access non-existing reorder buffer entry.\n";
+				exit(EXIT_FAILURE);
+			}
+			_buffer[target]->ready = true;
+			if (auto* branch_entry = dynamic_cast<Branch_Conditional_Reorder_Buffer_Entry*>(_buffer[target].get())) {
+				branch_entry->mispredicted = was_misprediction;
+			}
+			else {
+				std::cout << "Tried to access branch reorder buffer with wrong index.\n";
+				exit(EXIT_FAILURE);
+			}
+
+		}
+
+		void Reorder_Buffer::commit() {
+			for (size_t i{}; i < Constants::COMMIT_WIDTH; i++) {
+				auto* entry = _buffer[_head].get();
+				if (entry == nullptr) return;
+				if (!entry->ready) return;
+				switch (entry->flow()) {
+				case FrontEnd::FLOW_TYPE::REGISTER:
+				case FrontEnd::FLOW_TYPE::LOAD: {
+					auto entry_{ dynamic_cast<Register_Reorder_Buffer_Entry*>(entry) };
+					Register_File::deallocate((entry_)->old_alias);
+					Register_File::update_retirement_alias_table_with(
+						(entry_)->architectural_register_id,
+						(entry_)->new_alias
+					);
+					_buffer[_head].reset();
+					_head = (_head + 1) % Constants::REORDER_BUFFER_SIZE;
+					break;
+				}
+				case FrontEnd::FLOW_TYPE::STORE:
+					Execution_Unit_Load_Store::execute_store(dynamic_cast<Store_Reorder_Buffer_Entry*>(entry)->store_id);
+					_buffer[_head].reset();
+					_head = (_head + 1) % Constants::REORDER_BUFFER_SIZE;
+					break;
+				case FrontEnd::FLOW_TYPE::BRANCH_CONDITIONAL:
+
+					if (dynamic_cast<Branch_Conditional_Reorder_Buffer_Entry*>(entry)->mispredicted) {
+						ROB_MISPREDICTION_RECOVERY(Branch_Conditional_Reorder_Buffer_Entry)
+					}
+					else {
+						_buffer[_head].reset();
+						_head = (_head + 1) % Constants::REORDER_BUFFER_SIZE;
+					}
+					break;
+				case FrontEnd::FLOW_TYPE::BRANCH_UNCONDITIONAL: {
+					auto* entry_{ dynamic_cast<Branch_Unconditional_Reorder_Buffer_Entry*>(entry) };
+					Register_File::deallocate((entry_)->old_alias);
+					Register_File::update_retirement_alias_table_with(
+						(entry_)->architectural_register_id,
+						(entry_)->new_alias
+					);
+					if ((entry_)->mispredicted) {
+						ROB_MISPREDICTION_RECOVERY(Branch_Unconditional_Reorder_Buffer_Entry)
+					}
+					else {
+						_buffer[_head].reset();
+						_head = (_head + 1) % Constants::REORDER_BUFFER_SIZE;
+					}
+					break;
+
+				}
+				default:
+					break;
+
+				}
+			}
+		}
+		bool Reorder_Buffer::full() {
+			return (_tail + 1)  % Constants::REORDER_BUFFER_SIZE == _head;
+		}
+
+	} // namespace Core
+} // namespace OoOVis
