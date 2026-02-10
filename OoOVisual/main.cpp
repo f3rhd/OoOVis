dafeafb39ca05c8e4734586adbf43ce19f3a9bb2
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-11 01:08:04 +0400

    Refactored the main based on new mechanism

--- OoOVisual/main.cpp
+++ OoOVisual/main.cpp
@@ -12,19 +12,11 @@ int main(int argc, char** argv) {
 	auto parse_result(parser.parse_instructions(cli_args.input_file));
 	Fetch_Unit::init(std::move(parse_result.first));
 	Register_File::init();
-	Dispatcher dispatcher;
 	while (true) {
 		Reorder_Buffer::commit();
 		Execution_Stage::issue_and_execute();
 		memory_addr_t successful_dispatches{};
-		for (const auto& fetch_element : Fetch_Group::group) {
-			if (!Reorder_Buffer::full()) {
-				if (dispatcher.dispatch_instruction(fetch_element))
-					successful_dispatches++;
-			}
-		}
-		if(!Fetch_Unit::next_fetch_is_set())
-			Fetch_Unit::adjust_program_counter_based_on_successful_dispatches(successful_dispatches);
-		Fetch_Group::group = (Fetch_Unit::fetch());
+		auto feedback(Dispatcher::dispatch_fetch_group());
+		Fetch_Group::group = (Fetch_Unit::fetch(feedback));
 	}
 }
