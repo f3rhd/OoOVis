d2c7ee155b5387ae8fea34721a34101ee16e44a0
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-18 20:01:11 +0400

    add asset

--- OoOVisual/Visualizer/src/DrawElementWire.cpp
+++ OoOVisual/Visualizer/src/DrawElementWire.cpp
@@ -10,42 +10,7 @@ namespace OoOVisual
 	namespace Visualizer
 	{
 
-		bool Draw_Element_Wire::is_hovered(const Camera& cam) const 
-		{
-			ImVec2 s{ cam.world_to_screen(_start) };
-			ImVec2 e{ cam.world_to_screen(_end) };
-			ImVec2 m{ ImGui::GetIO().MousePos };
-
-			float l2{ (s.x - e.x) * (s.x - e.x) + (s.y - e.y) * (s.y - e.y) };
-			if (l2 == 0.0f) return false; 
-
-			float t{ ((m.x - s.x) * (e.x - s.x) + (m.y - s.y) * (e.y - s.y)) / l2 };
-			t = ImMax(0.0f, ImMin(1.0f, t)); 
-
-			ImVec2 projection{ s.x + t * (e.x - s.x), s.y + t * (e.y - s.y) };
-			float distSq{ (m.x - projection.x) * (m.x - projection.x) + (m.y - projection.y) * (m.y - projection.y) };
-
-			return distSq < (5.0f * 5.0f);
-
-		}
-
-		void Draw_Element_Wire::glow(const Camera& cam) const {
-			ImDrawList* draw_list{ ImGui::GetWindowDrawList() };
-			
-			ImVec2 s{ cam.world_to_screen(_start) };
-			ImVec2 e{ cam.world_to_screen(_end) };
-
-			float time{ (float)ImGui::GetTime() };
-			float pulse{ (sinf(time * 3.5f) + 1.0f) * 0.5f }; 
-
-			ImColor aura_color{ ImColor(0.6f, 0.3f, 1.0f, 0.05f + (pulse * 0.15f)) };
-			ImColor core_color{ ImColor(0.8f, 0.6f, 1.0f, 0.8f + (pulse * 0.2f)) };
-
-			draw_list->AddLine(s, e, aura_color, 12.0f); 
-			
-			draw_list->AddLine(s, e, core_color, 4.0f);  
-		}
-
+		
 		static void pulser(Draw_Element_Wire& wire) {
 			constexpr ImColor color_default { 255, 255, 255, 255 };      
 			constexpr ImColor color_success { 50, 255, 130, 255 };     
