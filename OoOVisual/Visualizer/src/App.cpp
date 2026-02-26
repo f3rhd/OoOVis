a521b91a64a581e291393f7d2774cc79cd18fe68
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-02-26 15:01:58 +0400

    lil fix

--- OoOVisual/Visualizer/src/App.cpp
+++ OoOVisual/Visualizer/src/App.cpp
@@ -39,12 +39,12 @@ namespace OoOVisual
 		}
 
 		void App::start_frame() {
-			ImGui::SetNextWindowSize(window_size());
 			glfwPollEvents();
 			
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
+			ImGui::SetNextWindowSize(window_size());
 		}
 
 		void App::end_frame() {
